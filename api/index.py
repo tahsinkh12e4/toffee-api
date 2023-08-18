b6f692268f2960081cc6d1a4d1cf1603fc89e472
tahsinkh12e4 <138883367+tahsinkh12e4@users.noreply.github.com>
2023-08-18 15:02:53 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,33 +1,76 @@
-from flask import Flask, make_response
+from flask import Flask, request, make_response
 import requests
-import os
 
 app = Flask(__name__)
 
-API_ENDPOINT = "https://bldcmprod-cdn.toffeelive.com/"
+cookie = "Edge-Cache-Cookie=URLPrefix=aHR0cHM6Ly9ibGRjbXByb2QtY2RuLnRvZmZlZWxpdmUuY29tLw:Expires=1693089595:KeyName=prod_linear:Signature=aC63Wde-Xt3uBt1I0nmq5QNaFjFp4o54zZK6Fa-ui-E6Mqj2mzumQLGRzXVinMIDpRwsaS-NwxkI_bzzxohADQ"
+
 headers = {
-    "Cookie": "Edge-Cache-Cookie=URLPrefix=aHR0cHM6Ly9ibGRjbXByb2QtY2RuLnRvZmZlZWxpdmUuY29tLw:Expires=1693089595:KeyName=prod_linear:Signature=aC63Wde-Xt3uBt1I0nmq5QNaFjFp4o54zZK6Fa-ui-E6Mqj2mzumQLGRzXVinMIDpRwsaS-NwxkI_bzzxohADQ"
+"cookie": cookie
 }
+
+base_url = "https://bldcmprod-cdn.toffeelive.com"
+
 
 @app.route("/")
 def credit():
-    return "(Toffee-API) Made For ToffeeLiveSetup "
+	
+	
+	return "Made With  By Proximity BD"
+
 
 @app.route("/auto/<string:channel_id>.m3u8")
-def handle_auto(channel_id):
-    response = requests.get(API_ENDPOINT + f"cdn/live/{channel_id}/playlist.m3u8", headers=headers)
-    myresponse = make_response(response.text.replace("../slang/", "/single/slang/").replace("?", "-"))
-    myresponse.headers["Content-Type"] = "application/vnd.apple.mpegurl"
-    return myresponse
+def handle_api(channel_id):
+	print(channel_id)
 
-@app.route("/single/<path:path>")
-def handle_single(path):
-    single_url = API_ENDPOINT + "cdn/live/" + path.replace("-", "?")
-    print(single_url)
-    response = requests.get(single_url, headers=headers)
-    myresponse = make_response(response.text.replace("/live/", f"{API_ENDPOINT}/live/"))
-    myresponse.headers["Content-Type"] = "application/vnd.apple.mpegurl"
-    return myresponse
+ # Retrieve the m3u8 content
+	m3u8_url = f"https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/{channel_id}_576/{channel_id}_576.m3u8?bitrate=1000000&channel={channel_id}_576&gp_id="
+	response = requests.get(m3u8_url, headers=headers)
+	lines = response.text.splitlines()
+
+ # Modify the playlist to include the /ts endpoint for each .ts segment
+	ts_urls = []
+	for line in lines:
+		if ".ts" in line:
+			ts_urls.append(f"/ts?id={line}&base={base_url}")
+		else:
+			ts_urls.append(line)
+
+	m3u8_content = "\n".join(ts_urls)
+	specified_text_prefix = "/file"
+	specified_text_suffix = ".key"
+	replacement_text = f"/key?id=/file.key" # Replace with your specified text
+
+	start_index = m3u8_content.find(specified_text_prefix)
+	end_index = m3u8_content.find(specified_text_suffix, start_index)
+	specified_text = m3u8_content[start_index: end_index + len(specified_text_suffix)]
+
+	result = m3u8_content.replace(specified_text, replacement_text)
+
+ # Create a response with the updated M3U8 playlist
+	response = make_response(result)
+	response.headers.get("content-type")
+	return response
+
+
+@app.route("/ts")
+def handle_ts():
+	# Handler for serving individual TS segments
+	ts_id = request.args.get("id")
+	base = request.args.get("base")
+	if not ts_id or not base:
+		return "Please provide both 'id' and 'base' parameters in the URL query"
+
+ # Construct the URL for the TS segment
+	ts_url = base + ts_id
+
+	# Fetch the TS segment content
+	response = requests.get(ts_url, headers=headers)
+
+ # Create a response with the TS segment content
+	myresponse = make_response(response.content)
+	myresponse.headers.get("content-type") # Using this to get the content type directly from the headers
+	return myresponse
 
 
 @app.route("/key")
@@ -56,4 +99,4 @@ def set_cookie():
  		return "Please provide a 'cookie' parameter in the URL query"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
